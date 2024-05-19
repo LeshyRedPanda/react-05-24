3cc4f7caf01171cfde4523b9324dcf944ab131d3
LeshyRedPanda <sokolov1993@gmail.com>
2024-05-19 19:41:19 +0300

    hw 6 routes end + errorElement.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import HomePage from "../pages/HomePage";
 import UsersComponent from "../components/UsersComponent";
 
+
 export const routerConfig = createBrowserRouter([
         // {path: '/', element: <MainLayout/>},
         // {path: '/ok', element: <h2>OK OK OK OK PAGE 2 </h2>},
@@ -13,6 +14,7 @@ export const routerConfig = createBrowserRouter([
         // {path: '/babalymaga', element: <h2>babalymaga babalymaga babalymaga PAGE 6 </h2>},
         {path: '/',
             element: <MainLayout/>,
+            errorElement:<h1> Errooroo</h1>,
             children:[      //children is Outlet in Layout
                 // {path:'home',element:<HomePage/>},      // path to children without '/'
                 {index:true,element:<HomePage/>},      // index:true - makes this page default page
