fc9e506d0054b8342264c3247907a68ffee31471
LeshyRedPanda <sokolov1993@gmail.com>
2024-06-08 17:49:00 +0300

    hw-11  end .

--- src/context/ContextProvider.ts
+++ src/context/ContextProvider.ts
@@ -4,84 +4,71 @@ import {ICommentModel} from "../models/ICommentModel";
 import {create} from "zustand";
 
 type StoreType = {
-    userStore: { allUsers: IUserModel[]
-    loadUsers: (user: IUserModel[]) => void,
+    userStore: {
+        allUsers: IUserModel[]
+        loadUsers: (user: IUserModel[]) => void,
     },
-    postsStore: { allPosts: IPostModel[]
-    loadPosts: (post: IPostModel[]) => void,
+    postsStore: {
+        allPosts: IPostModel[]
+        loadPosts: (post: IPostModel[]) => void,
     },
-    commentsStore: { allComments: ICommentModel[]
-    loadComments: ( comment: ICommentModel[] ) => void ,
+    commentsStore: {
+        allComments: ICommentModel[]
+        loadComments: (comment: ICommentModel[]) => void,
     },
 }
-
-// const defaultValue: StoreType = {
-//     userStore: {allUsers: []},
-//     postsStore: {allPosts: []},
-//     commentsStore: {allComments: []},
-// }
-
-// const Context = createContext<StoreType>(defaultValue)
-//
-//
-// const useContextProvider = (): StoreType => {
-//     return useContext(Context)
-// };
 
 const useStore = create<StoreType>()((set) => {
     return {
         userStore: {
-                allUsers: [],
-                // loadUsers - function set "users[]"
-                loadUsers: (users: IUserModel[]) => {
-                    return set((state:StoreType) => {
-                        return{
-                            ...state,
-                            userStore:{
-                                ...state.userStore,
-                                allUsers:users
-                            }
+            allUsers: [],
+            // loadUsers - function set "users[]"
+            loadUsers: (users: IUserModel[]) => {
+                return set((state: StoreType) => {
+                    return {
+                        ...state,
+                        userStore: {
+                            ...state.userStore,
+                            allUsers: users
                         }
-                    })
-                }
-            },
-        postsStore: {
-                allPosts: [],
-                // loadPosts - function set "posts[]"
-                loadPosts: (posts:IPostModel[]) => {
-                    return set((state:StoreType) => {
-                        return{
-                            ...state,
-                            postsStore:{
-                                ...state.postsStore,
-                                allPosts:posts
-                            }
-                        }
-                    })
-                }
-            },
-        commentsStore: {
-                allComments: [],
-                // loadComments - function set "comments[]"
-                loadComments: (comments:ICommentModel[]) => {
-                    return set((state:StoreType) => {
-                        return{
-                            ...state,
-                            commentsStore:{
-                                ...state.commentsStore,
-                                allComments:comments
-                            }
-                        }
-                    })
-                }
+                    }
+                })
             }
+        },
+        postsStore: {
+            allPosts: [],
+            // loadPosts - function set "posts[]"
+            loadPosts: (posts: IPostModel[]) => {
+                return set((state: StoreType) => {
+                    return {
+                        ...state,
+                        postsStore: {
+                            ...state.postsStore,
+                            allPosts: posts
+                        }
+                    }
+                })
+            }
+        },
+        commentsStore: {
+            allComments: [],
+            // loadComments - function set "comments[]"
+            loadComments: (comments: ICommentModel[]) => {
+                return set((state: StoreType) => {
+                    return {
+                        ...state,
+                        commentsStore: {
+                            ...state.commentsStore,
+                            allComments: comments
+                        }
+                    }
+                })
+            }
+        }
     }
 });
 
 
 export {
-    // Context,
-    // useContextProvider,
-    // defaultValue,
     useStore
 }
